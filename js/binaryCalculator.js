2563edaf7b7b76448c9ba75b3fe54e89624bf3c7
joseahb <biwottjoseah@gmail.com>
2021-07-01 12:23:39 +0300

    Change to use ParseInt base 2

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -53,9 +53,29 @@ function calculate (operator, operands) {
     
     switch (String(operator)) {
         case "+":
-            result = addBinary(operands[0],operands[1])
+            var x = parseInt(operands[0], 2);
+            var y = parseInt(operands[1], 2);
+            var sum = x + y;
+            result = sum.toString(2);
             break;
-    
+        case "-":
+            var x = parseInt(operands[0], 2);
+            var y = parseInt(operands[1], 2);
+            var sub = x - y;
+            result = sub.toString(2);
+            break;
+        case "*":
+            var x = parseInt(operands[0], 2);
+            var y = parseInt(operands[1], 2);
+            var mul = x * y;
+            result = mul.toString(2);
+            break;
+        case "/":
+            var x = parseInt(operands[0], 2);
+            var y = parseInt(operands[1], 2);
+            var div = x / y;
+            result = div.toString(2);
+            break;
         default:
             console.log("Oops")
             break;
@@ -63,43 +83,3 @@ function calculate (operator, operands) {
 
     return result;
 }
-
-function xor(a, b){return (a === b ? 0 : 1);}
-function and(a, b){return a == 1 && b == 1 ? 1 : 0;}
-function or(a, b){return (a || b);}
-
-function halfAdder(a, b){
-    const sum = xor(a,b);
-    const carry = and(a,b);
-    return [sum, carry];
-}
-
-function fullAdder(a, b, carry){
-    halfAdd = halfAdder(a,b);
-    const sum = xor(carry, halfAdd[0]);
-    carry = and(carry, halfAdd[0]);
-    carry = or(carry, halfAdd[1]);
-    return [sum, carry];
-}
-
-function addBinary(a, b){
-
-    let sum = '';
-    let carry = '';
-  
-    for(var i = a.length-1;i>=0; i--){
-      if(i == a.length-1){
-        //half add the first pair
-        const halfAdd1 = halfAdder(a[i],b[i]);
-        sum = halfAdd1[0]+sum;
-        carry = halfAdd1[1];
-      }else{
-        //full add the rest
-        const fullAdd = fullAdder(a[i],b[i],carry);
-        sum = fullAdd[0]+sum;
-        carry = fullAdd[1];
-      }
-    }
-  
-    return carry ? carry + sum : sum;
-  }
